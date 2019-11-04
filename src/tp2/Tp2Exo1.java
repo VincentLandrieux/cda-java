5c0df79dbe89c3824bf65ad053026febfb836209
Landrieux Vincent <vincent.landrieux@gmail.com>
2019-11-04 14:12:02 +0100

    Update Tp2Exo1.java

--- src/tp2/Tp2Exo1.java
+++ src/tp2/Tp2Exo1.java
@@ -1,9 +1,29 @@
 package tp2;
 
+import java.util.Scanner;
+
+/*
+ * Nom du répertoire: tp2
+ * Nom du projet: exo1
+ * Nom de la classe: Tp2Exo1
+ */
 public class Tp2Exo1 {
 
 	public static void main(String[] args) {
-		System.out.print("TP2");
+		
+		Scanner inp = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		System.out.println("Entrer un entier");
+		a = inp.nextInt();
+		System.out.println("Entrer un deuxième entier");
+		b = inp.nextInt();
+		
+		inp.close();
+		
+		System.out.println("Somme: " + (a+b));
 	}
 
 }
