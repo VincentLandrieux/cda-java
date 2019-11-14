2c6b17d7ab25f7fb04411ed001e7c090006b1ae0
Landrieux Vincent <vincent.landrieux@gmail.com>
2019-11-14 10:52:02 +0100

    Add files via upload

--- src/tp4/Tp4Exo8.java
+++ src/tp4/Tp4Exo8.java
@@ -3,8 +3,37 @@ package tp4;
 public class Tp4Exo8 {
 
 	public static void main(String[] args) {
-		System.out.println("result "+fibonacci(10));
+//		System.out.println("result "+fibonacci(10));
 
+		int n = 1;
+		float f[] = new float[3];
+		f[0] = fibonacci(n);
+		f[1] = fibonacci(n-1);
+		f[2] = f[0] / f[1];
+		
+		float limiteMin = 1.6179f;
+		float limiteMax = 1.6181f;
+		float nbOr = ((float)Math.sqrt(5)+1)/2;
+		boolean flag = true;
+		
+		while(flag){
+			n++;
+			
+			f[0] = fibonacci(n);
+			f[1] = fibonacci(n-1);
+			f[2] = f[0] / f[1];
+			
+			System.out.println("n= " + n);
+			System.out.println("f(n)= "+ f[2]);
+			
+			if(f[2] > limiteMin && f[2] < limiteMax) {
+				flag = false;
+			}
+			
+		}
+		
+		System.out.println("nombre d'or = "+nbOr+ " " +"f("+n+")= "+f[2]);
+		
 	}
 	
 	public static int fibonacci(int n) {
@@ -14,5 +43,4 @@ public class Tp4Exo8 {
 		
 		return n;
 	}
-
 }
